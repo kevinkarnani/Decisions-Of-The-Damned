870bff17cce836bf9a90a9cc2ba7e77eb42b8a3b
kk3286 <kk3286@cs.drexel.edu>
2019-04-25 20:44:59 -0400

    Document Choice class

--- Labwork/Project/TBD TBG/Choice.cs
+++ Labwork/Project/TBD TBG/Choice.cs
@@ -9,19 +9,22 @@ namespace TBD_TBG
         //TODO: ADD Combat value Yes/No for every decision
         //      ADD morality value for every decision
 
-        public string Description { get; set; }
-        public Dictionary<string, Choice> Choices { get; set; }
+        public string Description { get; set; } // Initialized Description property by using getter and setter
+        public Dictionary<string, Choice> Choices { get; set; } // Initialized Choices property by using getter and setter
+
+        //Class Constructor, takes in description parameter and sets it to class instance of Description
         public Choice(string des)
         {
             this.Description = des;
         }
 
-
+        // setter method for Choices Dictionary, takes a dictionary of strings and choices, sets it to class instance
         public void SetChoices(Dictionary<string, Choice> choices)
         {
             this.Choices = choices;
         }
 
+        //returns the keys of the dictionary as a string
         public string GetChoiceText()
         {
             string output = "";
@@ -32,11 +35,13 @@ namespace TBD_TBG
             return output;
         }
 
+        //returns true if there exists any number of objects greater than 0 in the dictionary
         public bool CheckChoice()
         {
             return (this.Choices.Count > 0);
         }
 
+        //parses string into int, then returns the values of the dictionary as a choice object
         public Choice GetChoice(string selection)
         {
             int num = Int32.Parse(selection) - 1;
