20f296d014006c4279469d95b4cb9a637b281fd1
kk3286 <kk3286@cs.drexel.edu>
2019-06-06 20:27:45 -0400

    Document ItemParser class

--- Labwork/Project/TBD TBG/Inventory and items/ItemParser.cs
+++ Labwork/Project/TBD TBG/Inventory and items/ItemParser.cs
@@ -6,31 +6,35 @@ namespace TBD_TBG
 {
     class ItemParser
     {
-        public static Dictionary<string, Item> GlobalItems = new Dictionary<string, Item>();
+        public static Dictionary<string, Item> GlobalItems = new Dictionary<string, Item>(); //dictionary to hold Items based on their ID
+
+        /*
+         * This function will parse the CSV containing all the items
+         */
         public static void Parser()
         {
-            string path = "CSV-Items.csv";
-            FileStream fileStream = new FileStream(path, FileMode.Open);
-            using (StreamReader reader = new StreamReader(fileStream))
-            using (CsvReader csv = new CsvReader(reader))
+            string path = "CSV-Items.csv"; //file path
+            FileStream fileStream = new FileStream(path, FileMode.Open); //create File object
+            using (StreamReader reader = new StreamReader(fileStream)) //read text
+            using (CsvReader csv = new CsvReader(reader)) //parse text as a CSV
             {
-                var records = csv.GetRecords<ItemStats>();
+                var records = csv.GetRecords<ItemStats>(); //Get all records in the CSV as a Enumerable of ItemStats
                 foreach (ItemStats record in records)
                 {
-                    if (record.GetItemType() == "Equibable")
+                    if (record.GetItemType() == "Equibable") //if item can be equipped
                     {
-                        Equipable item = new Equipable(record.GetID(), record.GetName(), record.GetDes());
-                        item.SetItemStats(record.GetAgility(), record.GetAttack(), record.GetHP());
-                        item.SetLocation(record.GetLocDes());
-                        GlobalItems.Add(record.GetID(), item);
+                        Equipable item = new Equipable(record.GetID(), record.GetName(), record.GetDes()); //create Equipable object
+                        item.SetItemStats(record.GetAgility(), record.GetAttack(), record.GetHP()); //set Equipable item's stats
+                        item.SetLocation(record.GetLocDes()); //set location of item
+                        GlobalItems.Add(record.GetID(), item); //append item to dictionary based on ID
                     }
-                    else if (record.GetItemType() == "Consumable")
+                    else if (record.GetItemType() == "Consumable") //if item can be consumed
                     {
-                        Consumable item = new Consumable(record.GetID(), record.GetName(), record.GetDes());
-                        item.SetItemStats(record.GetAgility(), record.GetAttack(), record.GetHP());
-                        item.SetLocation(record.GetLocDes());
-                        item.SetUsability(record.GetCombatUsability());
-                        GlobalItems.Add(record.GetID(), item);
+                        Consumable item = new Consumable(record.GetID(), record.GetName(), record.GetDes()); //create Consumable object
+                        item.SetItemStats(record.GetAgility(), record.GetAttack(), record.GetHP()); //set Consumable item's stats
+                        item.SetLocation(record.GetLocDes()); //set location of item
+                        item.SetUsability(record.GetCombatUsability()); //determine if Consumable item can be used outside of battle
+                        GlobalItems.Add(record.GetID(), item); //append to dictionary based on ID
                     }
                 }
             }
