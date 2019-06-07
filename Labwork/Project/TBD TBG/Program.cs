f5fc70f98bd2692f8a822519da45bcc2c8b0b76a
Mark Melkumyan <mam962@drexel.edu>
2019-06-06 23:04:43 -0400

    added more comments

--- Labwork/Project/TBD TBG/Program.cs
+++ Labwork/Project/TBD TBG/Program.cs
@@ -4,8 +4,6 @@ namespace TBD_TBG
 {
     class Program
     {
-        //TODO: start all methods with capital letters
-
         //main method, calls most important functions
         static void Main()
         {
